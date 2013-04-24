e55d6ac406f58281a1f13b66ae5cd5b6fdad8504
Bartek Ciszkowski <bart.ciszk@gmail.com>
2013-04-24 13:20:23 -0400

    Update packag name for the cheese shop

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gapipy',
+    name='gapi-python',
     version='0.0.3',
     author='G Adventures',
     author_email='software@gadventures.com',
